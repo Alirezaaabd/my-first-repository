187fdfeb183ceb7b8acba322af162ec210b11b91
Alirezaaabd <112006782+Alirezaaabd@users.noreply.github.com>
2022-08-24 10:22:07 -0700

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,18 @@
+def is_palindrome(teststr):
+    if teststr == teststr[::-1]:
+        return True
+    return False
+
+run = True
+while (run):
+    teststr = input("Please enter string to test for palindrome or 'exit': ")
+    if teststr == "exit":
+        run = False
+        break
+    teststr = teststr.lower()
+    #strip all the spaces and punctuation from the string
+    newstr = ""
+    for x in teststr:
+        if x.isalnum():
+            newstr += x
+    print("Palindrome test:", is_palindrome(newstr))
